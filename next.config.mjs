5c42cf0e1dd8ddd9fdf366ea1f86ea7f10fd9b99
TrivenaPanjaitan <trivena.panjaitan@ist.id>
2024-07-27 00:29:52 +0700

    fixing

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // output: process.env.NEXT_PUBLIC_ENV === "production" ? "export" : undefined,
   output: "export",
-  basePath:
-    process.env.NEXT_PUBLIC_ENV === "production"
-      ? "/CodeChallange2"
-      : undefined,
-  assetPrefix:
-    process.env.NEXT_PUBLIC_ENV === "production"
-      ? "/CodeChallange2/"
-      : undefined,
-  trailingSlash:
-    process.env.NEXT_PUBLIC_ENV === "production" ? true : undefined,
+  trailingSlash: true,
+  // basePath:
+  //   process.env.NEXT_PUBLIC_ENV === "production"
+  //     ? "/CodeChallange2"
+  //     : undefined,
+  // assetPrefix:
+  //   process.env.NEXT_PUBLIC_ENV === "production"
+  //     ? "/CodeChallange2/"
+  //     : undefined,
+  // trailingSlash:
+  //   process.env.NEXT_PUBLIC_ENV === "production" ? true : undefined,
 };
 
 export default nextConfig;
